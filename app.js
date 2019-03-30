f76a3d944a9bb34dd66ce8bc2237993a93ca4229
themse <den.polusmyak@gmail.com>
2019-03-30 11:47:46 +0200

    implemented models

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
+const morgan = require('morgan');
 
 const authRoutes = require('./routes/auth');
 const analyticsRoutes = require('./routes/analytics');
@@ -9,6 +11,8 @@ const positionRoutes = require('./routes/position');
 
 const app = express();
 
+app.use(morgan('dev'));
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
